1c06af8996ba720d04a63256973e554c76c8d752
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-11 23:07:47 +0100

    constants: Lint the code, fix pi

--- constants.f90
+++ constants.f90
@@ -1,14 +1,17 @@
 module constants
-  private
-  integer, parameter :: xp = selected_real_kind(8)
-  real(kind = xp), parameter    :: pi = 3.14159265359_xp
-  real(kind = xp), parameter    :: G = 1._xp
+   implicit none
 
-  real(kind = xp)    :: epsilon2
-  integer            :: npoints
+   private
 
-  public :: xp, pi, G, epsilon2, npoints
-  
+   integer,         parameter    :: xp = selected_real_kind(8)
+   real(kind = xp), parameter    :: pi = 4.0_xp*atan(1.0_xp)
+   real(kind = xp), parameter    :: G  = 1._xp
+
+   real(kind = xp) :: epsilon2
+   integer         :: npoints
+
+   public :: xp, pi, G, epsilon2, npoints
+
 contains
-  
+
 end module constants
