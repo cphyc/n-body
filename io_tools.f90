6b240250d45ef7da46209fd4c94be9ab6f9a5e57
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-11 23:08:18 +0100

    io_tools: Lint the code

--- io_tools.f90
+++ io_tools.f90
@@ -1,55 +1,63 @@
 module io_tools
-  use constants
+   use constants
 
-  implicit none
+   implicit none
 
-  private
+   private
 
-  public :: read_npoints, read_mpos, write_dump, write_dump_headers
+   public :: read_npoints, read_mpos, write_dump, write_dump_headers
 contains
-  
-  subroutine read_npoints (un)
-    integer, intent(in)  :: un
 
-    read (un, *) npoints
-  end subroutine read_npoints
-  
-  subroutine read_mpos(un, m, r)
-    integer, intent(in)                           :: un
-    real(kind = xp), dimension(:), intent(out)    :: m
-    real(kind = xp), dimension(:, :), intent(out) :: r
+   subroutine read_npoints (un)
+      implicit none
+      integer, intent(in)  :: un
 
-    integer                                       :: i = 0
-    
-    do i = 1, npoints
-       read (un, *) r(i, 1), r(i, 2), r(i, 3), m(i)
-    end do 
-  
-  end subroutine read_mpos
+      read (un, *) npoints
 
-  subroutine write_dump_headers (un_int)
-    integer, intent(in) :: un_int
+   end subroutine read_npoints
 
-    write (un_int, '(10(a16))') 'iter', 't', 'Ep', 'Ec', 'E'
-  end subroutine write_dump_headers
+   subroutine read_mpos(un, m, r)
+      implicit none
 
-  
-  subroutine write_dump (un_all, un_int, iter, Ec, Ep, E, t, r, v)
-    real(kind=xp), intent(in) :: Ep, Ec, E, t
-    real(kind=xp), intent(in), dimension(:,:) :: r
-    real(kind=xp), intent(in), dimension(:,:) :: v
-    integer, intent(in) :: un_all, un_int, iter
-    
-    integer :: i
-    write (un_int, '(i16, 10(e16.8e2))') iter, t, Ep, Ec, E
+      integer, intent(in) :: un
 
-!    write (un_all, *) iter
-!    write (un_all, '(10(a16))') 'x', 'y', 'z', 'vx', 'vy', 'vz'
-!    do i = 1, npoints
-!       write (un_all, '(10(e16.8e2))') r(i, 1), r(i, 2), r(i, 3), v(i, 1), v(i, 2), v(i, 3)
-!    end do
-    
+      real(kind = xp), dimension(:),    intent(out) :: m
+      real(kind = xp), dimension(:, :), intent(out) :: r
+
+      integer :: i = 0
+
+      do i = 1, npoints
+         read (un, *) r(i, 1), r(i, 2), r(i, 3), m(i)
+      end do
+
+   end subroutine read_mpos
+
+   subroutine write_dump_headers (un_int)
+      implicit none
+      integer, intent(in) :: un_int
+
+      write (un_int, '(10(a16))') 'iter', 't', 'Ep', 'Ec', 'E'
+
+   end subroutine write_dump_headers
+
+   subroutine write_dump (un_all, un_int, iter, Ec, Ep, E, t, r, v)
+      implicit none
+
+      real(kind=xp),                 intent(in) :: Ec, Ep, E, t
+      real(kind=xp), dimension(:,:), intent(in) :: r
+      real(kind=xp), dimension(:,:), intent(in) :: v
+      integer,                       intent(in) :: un_all, un_int, iter
+
+      integer :: i
+
+      write (un_int, '(i16, 10(e16.8e2))') iter, t, Ep, Ec, E
+
+!      write (un_all, *) iter
+!      write (un_all, '(10(a16))') 'x', 'y', 'z', 'vx', 'vy', 'vz'
+!      do i = 1, npoints
+!         write (un_all, '(10(e16.8e2))') r(i, 1), r(i, 2), r(i, 3), v(i, 1), v(i, 2), v(i, 3)
+!      end do
+
   end subroutine write_dump
 
-  
 end module io_tools
