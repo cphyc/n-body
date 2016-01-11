075a44ae3ce5173a24d95fbc16d34f1e6de52e87
Corentin Cadiou <contact@cphyc.me>
2016-01-11 17:32:32 +0100

    indent

--- constants.f90
+++ constants.f90
@@ -1,12 +1,13 @@
 module constants
   private
   integer, parameter :: xp = selected_real_kind(8)
-  real(kind = xp)    :: pi = 3.14159265359_xp
-  real(kind = xp)    :: G = 1._xp
+  real(kind = xp), parameter    :: pi = 3.14159265359_xp
+  real(kind = xp), parameter    :: G = 1._xp
 
-  integer :: npoints
+  real(kind = xp)    :: epsilon
+  integer            :: npoints
 
-  public :: xp, pi, G, npoints
+  public :: xp, pi, G, epsilon, npoints
   
 contains
   
