a7bd727064bf9126d5ba46aeba8f4e82180ee1a6
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-11 22:58:26 +0100

    io_tools: Don’t save r,v for now
    We only check energy, let’s save some IOs.

--- io_tools.f90
+++ io_tools.f90
@@ -43,11 +43,11 @@ contains
     integer :: i
     write (un_int, '(i16, 10(e16.8e2))') iter, t, Ep, Ec, E
 
-    write (un_all, *) iter
-    write (un_all, '(10(a16))') 'x', 'y', 'z', 'vx', 'vy', 'vz'
-    do i = 1, npoints
-       write (un_all, '(10(e16.8e2))') r(i, 1), r(i, 2), r(i, 3), v(i, 1), v(i, 2), v(i, 3)
-    end do
+!    write (un_all, *) iter
+!    write (un_all, '(10(a16))') 'x', 'y', 'z', 'vx', 'vy', 'vz'
+!    do i = 1, npoints
+!       write (un_all, '(10(e16.8e2))') r(i, 1), r(i, 2), r(i, 3), v(i, 1), v(i, 2), v(i, 3)
+!    end do
     
   end subroutine write_dump
 
