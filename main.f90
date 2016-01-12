59811b1c50a970c9088558eb4e602a24a3805af8
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-12 13:35:39 +0100

    main: Use static npoints, no dimension

--- main.f90
+++ main.f90
@@ -25,25 +25,25 @@ program n_body
    !---------------------------------------------
    call cpu_time(start_time)
 
-   !---------------------------------------------
-   ! Read number of points
-   !---------------------------------------------
-   open(unit=10, file='initial_conditions.dat')
-   call read_npoints(10)
-
-   !---------------------------------------------
-   ! Allocations
-   !---------------------------------------------
-   allocate(m(npoints))
-   allocate(r(npoints, 3))
-   allocate(v(npoints, 3))
-   allocate(a(npoints, 3))
+   real(kind = xp) :: m(npoints)        ! Masses of the particles
+   real(kind = xp) :: r(npoints,3)      ! Positions of the particles (3-dim vectors)
+   real(kind = xp) :: v(npoints,3)      ! Speeds of the particles (3-dim vectors)
+   real(kind = xp) :: a(npoints,3)      ! Acceleration of the particles (3-dim vectors)
+   real(kind = xp) :: t = 0._xp         ! Total time elapsed in the simulation
+   real(kind = xp) :: dt                ! Timestep
+   real(kind = xp) :: Ec                ! Total kinetic energy
+   real(kind = xp) :: Ep                ! Total potential energy
+   real(kind = xp) :: E                 ! Total energy
+   integer         :: iter      = 0     ! Number of iterations ran
+   integer         :: dump_freq = 10    ! Frequency at which the system is sampled
+   integer         :: maxiter   = 10000 ! Maximum number of iterations
 
    !---------------------------------------------
    ! Read initial positions
    !---------------------------------------------
-   call read_mpos(10, m, r)
-   close(unit=10)
+   open(newunit=un, file='initial_conditions.dat', status="old")
+   call read_mpos(un, m, r)
+   close(un)
 
    !---------------------------------------------
    ! Compute initial speeds, accelerations, variables
@@ -53,21 +53,22 @@ program n_body
    call compute_initial_variables()
 
    !---------------------------------------------
-   ! Compute time step
+   ! Compute time step as a fraction of dynamic time
    !---------------------------------------------
-   ! the timestep here is experimental and corresponds to 1% of the
-   ! dynamic time
-   dt = 1.e-2_xp
+   dt = 1.e-3_xp
    print *, dt
+
+   !---------------------------------------------
+   ! Open files for output, add headers
+   !---------------------------------------------
+   open(newunit=una, file='output.dat', status="replace")
+   open(newunit=un, file='output_int.dat', status="replace")
+
+   call write_dump_headers(un)
 
    !---------------------------------------------
    ! Loop over time
    !---------------------------------------------
-   open(unit=11, file='output.dat')
-   open(unit=12, file='output_int.dat')
-
-   call write_dump_headers(12)
-
    do while (iter < maxiter)
 
       call integrate(v, a, dt/2)  ! Compute v(t+dt/2)
@@ -81,20 +82,16 @@ program n_body
       if (mod(iter, dump_freq) == 0) then
          print*, 'Dump!'
          call compute_energy(m, r, v, Ec, Ep, E) ! Compute Ec, Ep, E at t+dt
-         call write_dump(11, 12, iter, Ec, Ep, E, t, r, v)
+         call write_dump(una, un, iter, Ec, Ep, E, t, r, v)
       end if
 
    end do
 
-   close(unit=11)
-   close(unit=12)
-
    !---------------------------------------------
-   ! Deallocations
+   ! Close files
    !---------------------------------------------
-   deallocate(r)
-   deallocate(v)
-   deallocate(a)
+   close(un)
+   close(una)
 
    !---------------------------------------------
    ! Elapsed time
