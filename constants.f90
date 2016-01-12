dbe01ea25e067a0f09bea879a96f2e86f29860d5
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-12 13:34:30 +0100

    constants: Add npoint, unit var

--- constants.f90
+++ constants.f90
@@ -6,11 +6,12 @@ module constants
    integer,         parameter    :: xp = selected_real_kind(8)
    real(kind = xp), parameter    :: pi = 4.0_xp*atan(1.0_xp)
    real(kind = xp), parameter    :: G  = 1._xp
+   integer,         parameter    :: npoints = 1000
 
+   integer         :: un, una  ! Unit numbers for file operation
    real(kind = xp) :: epsilon2
-   integer         :: npoints
 
-   public :: xp, pi, G, epsilon2, npoints
+   public :: xp, pi, G, npoints, epsilon2, un, una
 
 contains
 
