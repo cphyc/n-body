d61e1d150c9157138ba7b719f3957dc7adf948ad
Corentin Cadiou <contact@cphyc.me>
2016-01-13 13:34:07 +0100

    Use seq instead of zsh comprehension

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p tmp
-for i in {1..12}; do
+for i in $(seq 1 12); do
     sed "s/OMP_NUM_THREADS=.*/OMP_NUM_THREADS=$i/g" job.slurm |
 	sed "s/job-name=.*/job-name=batch-$i/g" > tmp/job$i.slurm
     sbatch tmp/job$i.slurm
