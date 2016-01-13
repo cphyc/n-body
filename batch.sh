1ce1834fd6a260f6d20621b2c2f84a9ddf8de283
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-13 09:36:55 +0100

    batch: Use range in loop

--- batch.sh
+++ batch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p tmp
-for i in 1 2 3 4 5 6 7 8 9 10 11 12; do
+for i in {1..12}; do
     sed "s/OMP_NUM_THREADS=.*/OMP_NUM_THREADS=$i/g" job.slurm |
 	sed "s/job-name=.*/job-name=batch-$i/g" > tmp/job$i.slurm
     sbatch tmp/job$i.slurm
