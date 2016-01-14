eb985df6d25cc1d4a2dc742a5147f8d7082894c5
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-14 10:50:19 +0100

    main: Add flags to select subroutines
    Lean the code a bit, make sure it works without mpi

--- main.f90
+++ main.f90
@@ -7,22 +7,26 @@ program n_body
 
    implicit none
 
-   real(kind = xp) :: m(npoints)            ! Masses of the particles
-   real(kind = xp) :: r(3,npoints)          ! Positions of the particles (3-dim vectors)
-   real(kind = xp) :: v(3,npoints)          ! Speeds of the particles (3-dim vectors)
-   real(kind = xp) :: a(3,npoints)          ! Acceleration of the particles (3-dim vectors)
+   real(kind = xp) :: m(npoints)                ! Masses of the particles
+   real(kind = xp) :: r(3,npoints)              ! Positions of the particles (3-dim vectors)
+   real(kind = xp) :: v(3,npoints)              ! Speeds of the particles (3-dim vectors)
+   real(kind = xp) :: a(3,npoints)              ! Acceleration of the particles (3-dim vectors)
    real(kind = xp) :: a_reduced(3,npoints)
-   real(kind = xp) :: t = 0._xp             ! Total time elapsed in the simulation
-   real(kind = xp) :: dt                    ! Timestep
-   real(kind = xp) :: Ec                    ! Total kinetic energy
-   real(kind = xp) :: Ep                    ! Total potential energy
-   real(kind = xp) :: E                     ! Total energy
-   integer         :: iter      = 0         ! Number of iterations ran
-   integer         :: dump_freq = 10        ! Frequency at which the system is sampled
-   real            :: maxtime = real(npoints)/10000 ! Maximum time (ad hoc)
+   real(kind = xp) :: t = 0._xp                 ! Total time elapsed in the simulation
+   real(kind = xp) :: dt                        ! Timestep
+   real(kind = xp) :: Ec                        ! Total kinetic energy
+   real(kind = xp) :: Ep                        ! Total potential energy
+   real(kind = xp) :: E                         ! Total energy
+   integer         :: iter      = 0             ! Number of iterations ran
+   integer         :: dump_freq = 10            ! Frequency at which the system is sampled
+   real            :: maxtime = npoints/128._xp ! Maximum time (ad hoc)
    integer         :: maxiter
+   integer         :: flag_compute_force = 1    ! Choose force computation subroutine, 0=sequential, 1=omp, 2=omp_nn_1
+   integer         :: flag_compute_energy = 2   ! Choose energy computation subroutine, 0=sequential, 1=omp, 2=omp_nn_1
 
-   integer         :: err, nprocs, rank = 0
+   integer         :: err = 0
+   integer         :: rank = 0
+   integer         :: nprocs = 1
    integer         :: MASTER = 0
 
    integer         :: istart, iend, domain_size
@@ -50,7 +54,18 @@ program n_body
    ! Compute initial speeds and accelerations
    !---------------------------------------------
    call initial_speeds(r, v)
-   call compute_force(m, r, a)
+   istart = 1
+   iend = npoints
+   select case (flag_compute_force)
+      case(0)
+         call compute_force(m, r, istart, iend, a)
+      case(1)
+         call compute_force_omp(m, r, istart, iend, a)
+      case(2)
+         call compute_force_omp_nn_1(m, r, istart, iend, a)
+      case default
+         stop "Unknown value of flag_compute_force"
+   end select
 
    !---------------------------------------------
    ! Set time step and output parameters
@@ -80,12 +95,22 @@ program n_body
 
       call integrate(v, a, dt/2)  ! Compute v(t+dt/2)
       call integrate(r, v, dt)    ! Compute r(t+dt)
-      !call compute_force(m, r, a) ! Compute a(t+dt)
 
-      ! get the domain for integration from the number of nodes
+      !--------------------------------
+      ! Get the domain for integration from the number of nodes
+      !--------------------------------
       istart = domain_size*rank + 1
       iend   = min(istart + domain_size - 1, npoints)
-      call compute_force_omp(m, r, istart, iend, a) ! Compute a(t+dt)
+      select case (flag_compute_force)
+         case(0)
+            call compute_force(m, r, istart, iend, a)          ! Compute a(t+dt) with sequential version
+         case(1)
+            call compute_force_omp(m, r, istart, iend, a)      ! Compute a(t+dt) with naive OpenMP version
+         case(2)
+            call compute_force_omp_nn_1(m, r, istart, iend, a) ! Compute a(t+dt) with fast OpenMP version
+         case default
+            stop "Unknown value of flag_compute_force"
+      end select
 
       !--------------------------------
       ! reduce accelerations
@@ -99,11 +124,22 @@ program n_body
       iter = iter + 1
 
       if (mod(iter, dump_freq) == 0 .and. rank == MASTER) then
+
          print *, 'Dump', iter, t
-         !call compute_energy(m, r, v, Ec, Ep, E) ! Compute Ec, Ep, E at t+dt
-         !call compute_energy_omp(m, r, v, Ec, Ep, E) ! Compute Ec, Ep, E at t+dt
-         call compute_energy_omp_nn_1(m, r, v, Ec, Ep, E) ! Compute Ec, Ep, E at t+dt
+
+         select case (flag_compute_energy)
+            case(0)
+               call compute_energy(m, r, v, Ec, Ep, E)          ! Compute Ec, Ep, E at t+dt with sequential version
+            case(1)
+               call compute_energy_omp(m, r, v, Ec, Ep, E)      ! Compute Ec, Ep, E at t+dt with naive OpenMP version
+            case(2)
+               call compute_energy_omp_nn_1(m, r, v, Ec, Ep, E) ! Compute Ec, Ep, E at t+dt with fast OpenMP version
+            case default
+               stop "Unknown value of flag_compute_energy"
+         end select
+
          call write_dump(una, un, iter, Ec, Ep, E, t, r, v)
+
       end if
 
    end do
@@ -120,4 +156,5 @@ program n_body
    ! Stop MPI
    !---------------------------------------------
    call mpi_finalize(err)
+
 end program n_body
