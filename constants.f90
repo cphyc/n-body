adfda9e096b401a9fbf8f0ea39201b19afbddf42
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-14 09:28:44 +0100

    constants: Fix epsilon2
    That was it

--- constants.f90
+++ constants.f90
@@ -7,7 +7,7 @@ module constants
    real(kind = xp), parameter :: pi = 4.0_xp*atan(1.0_xp)
    real(kind = xp), parameter :: G  = 1._xp
    integer,         parameter :: npoints = 2**13
-   real(kind = xp), parameter :: epsilon2 = npoints**(2._xp/3._xp) / 400._xp ! We take 1/20th of the initial caracteristic distance
+   real(kind = xp), parameter :: epsilon2 = npoints**(-2._xp/3._xp) / 400._xp ! We take 1/20th of the initial caracteristic distance
 
    integer :: un, una  ! Unit numbers for file operation
 
