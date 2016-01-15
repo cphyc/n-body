61ad0ea4326d397cca2957de03b9bc484b4cb9e4
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-15 14:05:37 +0100

    Move force calculation wrapper to a function
    Also start to modify main to adapt to the new logic

--- main.f90
+++ main.f90
@@ -11,26 +11,18 @@ program n_body
    real(kind = xp), allocatable :: r(:, :)               ! Positions of the particles (3-dim vectors)
    real(kind = xp), allocatable :: v(:, :)               ! Speeds of the particles (3-dim vectors)
    real(kind = xp), allocatable :: a(:, : )               ! Acceleration of the particles (3-dim vectors)
-   real(kind = xp), allocatable :: a_comm(:, :), a_right(:, :)
-   real(kind = xp), allocatable :: r_i(:, :), r_np_i(:, :), r_right(:, :)
-   real(kind = xp), allocatable :: a_reduced(:, :)
    real(kind = xp) :: Ec                         ! Total kinetic energy
    real(kind = xp) :: Ep                         ! Total potential energy
    real(kind = xp) :: E                          ! Total energy
 
    real(kind = xp) :: t  = 0._xp                 ! Total time elapsed in the simulation
    integer         :: iter = 0                   ! Number of iterations ran
-   integer         :: i                          ! Counter for elements
-   integer         :: N                          ! Length of each block
 
    integer         :: err = 0
    integer         :: rank = 0
-   integer         :: nprocs = 1
+   integer         :: nprocs = 1                 ! Assumed to be either 1 or pair
    integer         :: MASTER = 0
-   integer         :: stat(MPI_STATUS_SIZE)
-   integer         :: domain_size
-
-   integer         :: istart, iend, jstart, jend, istart2, iend2
+   integer         :: N                          ! Length of each block/domain
 
    !---------------------------------------------
    ! Initialize MPI
@@ -40,47 +32,49 @@ program n_body
    call mpi_comm_rank(mpi_comm_world, rank, err)
 
    !---------------------------------------------
-   ! Compute number of domains
+   ! Compute size of domains                     ! FIXME: Should not depend on flag, rest of code must adapt
    !---------------------------------------------
-   domain_size = ceiling(npoints * 1.0 / nprocs)
-   N = ceiling(1._xp * npoints / nprocs)
-
-   !---------------------------------------------
-   ! Allocate space
-   !---------------------------------------------
-   allocate(m(N))
-   allocate(r(3, N))
-   allocate(v(3, N))
-   allocate(a(3, N))
-   allocate(a_comm(3, N))
-   allocate(a_right(3, N))
-   allocate(a_reduced(3, N))
-   allocate(r_i(3, N))
-   allocate(r_np_i(3, N))
-   allocate(r_right(3, N))
-
-   !---------------------------------------------
-   ! Read initial positions
-   !---------------------------------------------
-   open(newunit=un, file='initial_conditions.dat', status="old")
-   call read_mpos(un, rank*N + 1, (rank+1)*N + 1, m, r)
-   close(un)
-
-   !---------------------------------------------
-   ! Compute initial speeds and accelerations
-   !---------------------------------------------
-   call initial_speeds(r, v)
-   istart = 1
-   iend = npoints
-   select case (flag_compute_force)
+   select case (flag_compute_mpi)
       case(0)
-         call compute_force(m, r, istart, iend, r, istart, iend, a)
+         N = ceiling(npoints * 0.5 / nprocs)
       case(1)
-         call compute_force_omp(m, r, istart, iend, r, istart, iend, a)
-      case(2)
-         call compute_force_omp_nn_1(m, r, istart, iend, r, istart, iend, a)
+         N = ceiling(npoints * 1.0 / nprocs)
       case default
-         stop "Unknown value of flag_compute_force"
+         stop "Unknown value of flag_compute_mpi"
+   end select
+
+   !---------------------------------------------
+   ! Allocate space                              ! FIXME: See above
+   !---------------------------------------------
+   select case (flag_compute_mpi)
+      case(0)
+         allocate(m(N*2))
+         allocate(r(3, N*2))
+         allocate(v(3, N*2))
+         allocate(a(3, N*2))
+      case(1)
+         allocate(m(N))
+         allocate(r(3, N))
+         allocate(v(3, N))
+         allocate(a(3, N))
+      case default
+         stop "Unknown value of flag_compute_mpi"
+   end select
+
+   !---------------------------------------------
+   ! Read initial positions                      ! FIXME: Not compatible with all code modes, and not really good in MPI_low
+   !---------------------------------------------
+   select case (flag_compute_mpi)
+      case(0)
+         open(newunit=un, file='initial_conditions.dat', status="old")
+         call read_mpos(un, 1, npoints, m, r)
+         close(un)
+      case(1)
+         open(newunit=un, file='initial_conditions.dat', status="old")
+         call read_mpos(un, rank*N + 1, (rank+1)*N + 1, m, r)
+         close(un)
+      case default
+         stop "Unknown value of flag_compute_mpi"
    end select
 
    !---------------------------------------------
@@ -104,28 +98,10 @@ program n_body
    end if
 
    !---------------------------------------------
-   ! Compute number of domains
-   !---------------------------------------------
-   domain_size = ceiling(npoints * 0.5 / nprocs)
-
-   !---------------------------------------------
    ! Compute initial speeds and accelerations
    !---------------------------------------------
    call initial_speeds(r, v)
-   istart = 1
-   iend = npoints
-   select case (flag_compute_force)
-      case(0)
-         call compute_force(m, r, istart, iend, r, istart, iend, a)
-      case(1)
-         call compute_force_diag(m, r, istart, iend/2, npoints, a)
-      case(2)
-         call compute_force_omp(m, r, istart, iend, r, istart, iend, a)
-      case(3)
-         call compute_force_omp_diag(m, r, istart, iend/2, npoints, a)
-      case default
-         stop "Unknown value of flag_compute_force"
-   end select
+   call compute_force_wrap(N, rank, nprocs, m, r, a)
 
    !---------------------------------------------
    ! Loop over time
@@ -135,102 +111,8 @@ program n_body
       call integrate(v, a, dt/2)  ! Compute v(t+dt/2)
       call integrate(r, v, dt)    ! Compute r(t+dt)
 
-      select case(flag_compute_mpi)
-         case(0)
-            !--------------------------------
-            ! Get the domain for integration from the number of nodes
-            !--------------------------------
-            istart = domain_size*rank + 1
-            iend   = min(istart + domain_size - 1, npoints/2)
-            istart2 = istart*2-1
-            iend2   = iend*2
-            select case (flag_compute_force)
-               case(0)
-                  call compute_force(m, r, istart2, iend2, r, istart2, iend2, a)          ! Compute a(t+dt) with sequential version
-               case(1)
-                  call compute_force_diag(m, r, istart, iend, npoints, a) ! Compute a(t+dt) with fast OpenMP version
-               case(2)
-                  call compute_force_omp(m, r, istart2, iend2, r, istart2, iend2, a)      ! Compute a(t+dt) with naive OpenMP version
-               case(3)
-                  call compute_force_omp_diag(m, r, istart, iend, npoints, a) ! Compute a(t+dt) with fast OpenMP version
-               case default
-                  stop "Unknown value of flag_compute_force"
-            end select
-         case(1)
-            !--------------------------------
-            ! Scatter positions across all processes
-            ! and compute interactions
-            !--------------------------------
-            a = 0._xp
-            N = ceiling(1._xp * npoints / nprocs)
-            do i = 1, nprocs / 2
-               ! Get data from nprocs-i in i
-               ! FIXME send r from right to left
-               call mpi_sendrecv(r, 3*N, MPI_REAL_XP, nprocs-i+1, 0, &
-                    r_right, 3*N, MPI_REAL_XP, i, 0, &
-                    MPI_COMM_WORLD, stat, err)
-
-               if (rank == i) then
-                  r_i = r
-               else if (rank == nprocs - i) then
-                  r_np_i = r
-               end if
-
-               ! Broadcast i-th data
-               call mpi_bcast(r_i, 3*N, MPI_REAL_XP, i, MPI_COMM_WORLD, err)
-               ! Broadcast n-i-th data
-               call mpi_bcast(r_np_i, 3*N, MPI_REAL_XP, nprocs - i, MPI_COMM_WORLD, err)
-
-               ! If i == rank, compute both diagonals
-               if (rank == i) then
-                  r_right = r_i
-                  call compute_force_diag(m, r, r, a)
-                  call compute_force_diag(m, r_right, r_right, a_right)
-                  a_comm = a
-
-                  ! compute on right side but communicate nothing
-               else if (rank < i) then
-                  call compute_force(m, r_np_i, r_right, a_right)
-                  a_comm = 0._xp
-
-                  ! compute on left side and communicate interaction
-               else
-                  call compute_force(m, r, r_i, a_comm)
-                  a = a - a_comm
-               end if
-
-               ! Receive interaction in i
-               call mpi_reduce(a_comm, a, 3*N, MPI_REAL_XP, MPI_SUM, i, MPI_COMM_WORLD, err)
-            end do
-
-            !--------------------------------
-            ! Gather missing data
-            !--------------------------------
-            do i = 1, nprocs / 2
-               if (rank == i .or. rank == nprocs - i + 1) then
-                  call mpi_sendrecv(a_right, 3*N, MPI_REAL_XP, i, 0, &
-                       a_comm, 3*N, MPI_REAL_XP, nprocs - i + 1, 0, &
-                       MPI_COMM_WORLD, stat, err)
-                  if (rank == nprocs - i) then
-                     a = a + a_comm
-                  end if
-               end if
-            end do
-
-            !--------------------------------
-            ! Update positions
-            !--------------------------------
-            call integrate(v, a, dt/2)
-
-         case default
-            stop "Unknown value of flag_compute_mpi"
-      end select
       !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FIX BELOW
-      !--------------------------------
-      ! reduce accelerations
-      !--------------------------------
-      call mpi_allreduce(a, a_reduced, npoints*3, MPI_REAL_XP, MPI_SUM, MPI_COMM_WORLD, err)
-      a = a_reduced
+      call compute_force_wrap(N, rank, nprocs, m, r, a)
 
       call integrate(v, a, dt/2)  ! Compute v(t+dt)
 
--- physics.f90
+++ physics.f90
@@ -1,12 +1,14 @@
 module physics
 use constants
 
+use mpi
+
 implicit none
 
 private
 
 public :: initial_speeds, integrate, integrate_omp, &
-          compute_force, compute_force_diag, compute_force_omp, compute_force_omp_diag, &
+          compute_force_wrap, &
           compute_energy, compute_energy_omp, compute_energy_omp_diag
 
 contains
@@ -203,6 +205,127 @@ contains
       !$OMP END PARALLEL
 
    end subroutine compute_force_omp_diag
+
+   subroutine compute_force_wrap (N, rank, nprocs, m, r, a)
+      implicit none
+
+      integer,  intent(in) :: N
+      integer,  intent(in) :: nprocs
+      integer,  intent(in) :: rank
+      real(xp), intent(in) :: m(:)
+      real(xp), intent(in) :: r(:,:)
+
+      real(xp), intent(out) :: a(:,:)
+
+      real(xp), allocatable :: a_comm(:, :), a_right(:, :)
+      real(xp), allocatable :: r_i(:, :), r_np_i(:, :), r_right(:, :)
+      real(xp), allocatable :: a_reduced(:, :)
+
+      integer         :: err = 0
+      integer         :: stat(MPI_STATUS_SIZE)
+      integer         :: i                          ! Counter for elements
+
+      integer         :: istart, iend, istart2, iend2
+!      integer         :: jstart, jend
+
+      allocate(a_comm(3, N))
+      allocate(a_right(3, N))
+      allocate(a_reduced(3, N))
+      allocate(r_i(3, N))
+      allocate(r_np_i(3, N))
+      allocate(r_right(3, N))
+
+      select case(flag_compute_mpi)
+         case(0)
+            !--------------------------------
+            ! Get the domain for integration from the number of nodes
+            !--------------------------------
+            istart = N*rank + 1
+            iend   = min(istart + N - 1, npoints/2)
+            istart2 = istart*2-1
+            iend2   = iend*2
+            select case (flag_compute_force)
+               case(0)
+                  call compute_force(m, r, istart2, iend2, r, istart2, iend2, a)          ! Compute a(t+dt) with sequential version
+               case(1)
+                  call compute_force_diag(m, r, istart, iend, npoints, a) ! Compute a(t+dt) with fast OpenMP version
+               case(2)
+                  call compute_force_omp(m, r, istart2, iend2, r, istart2, iend2, a)      ! Compute a(t+dt) with naive OpenMP version
+               case(3)
+                  call compute_force_omp_diag(m, r, istart, iend, npoints, a) ! Compute a(t+dt) with fast OpenMP version
+               case default
+                  stop "Unknown value of flag_compute_force"
+            end select
+               !--------------------------------
+               ! reduce accelerations
+               !--------------------------------
+               call mpi_allreduce(a, a_reduced, npoints*3, MPI_REAL_XP, MPI_SUM, MPI_COMM_WORLD, err)
+               a = a_reduced
+         case(1)
+            !--------------------------------
+            ! Scatter positions across all processes
+            ! and compute interactions
+            !--------------------------------
+            a = 0._xp
+            do i = 1, nprocs / 2
+               ! Get data from nprocs-i in i
+               ! FIXME send r from right to left
+               call mpi_sendrecv(r, 3*N, MPI_REAL_XP, nprocs-i+1, 0, &
+                    r_right, 3*N, MPI_REAL_XP, i, 0, &
+                    MPI_COMM_WORLD, stat, err)
+
+               if (rank == i) then
+                  r_i = r
+               else if (rank == nprocs - i) then
+                  r_np_i = r
+               end if
+
+               ! Broadcast i-th data
+               call mpi_bcast(r_i, 3*N, MPI_REAL_XP, i, MPI_COMM_WORLD, err)
+               ! Broadcast n-i-th data
+               call mpi_bcast(r_np_i, 3*N, MPI_REAL_XP, nprocs - i, MPI_COMM_WORLD, err)
+
+               ! If i == rank, compute both diagonals
+               if (rank == i) then
+                  r_right = r_i
+                  call compute_force_diag(m, r, 1, N, N, a)
+                  call compute_force_diag(m, r_right, 1, N, N, a_right)
+                  a_comm = a
+
+                  ! compute on right side but communicate nothing
+               else if (rank < i) then
+                  call compute_force(m, r_np_i, 1, N, r_right, 1, N, a_right)
+                  a_comm = 0._xp
+
+                  ! compute on left side and communicate interaction
+               else
+                  call compute_force(m, r, 1, N, r_i, 1, N, a_comm)
+                  a = a - a_comm
+               end if
+
+               ! Receive interaction in i
+               call mpi_reduce(a_comm, a, 3*N, MPI_REAL_XP, MPI_SUM, i, MPI_COMM_WORLD, err)
+            end do
+
+            !--------------------------------
+            ! Gather missing data
+            !--------------------------------
+            do i = 1, nprocs / 2
+               if (rank == i .or. rank == nprocs - i + 1) then
+                  call mpi_sendrecv(a_right, 3*N, MPI_REAL_XP, i, 0, &
+                       a_comm, 3*N, MPI_REAL_XP, nprocs - i + 1, 0, &
+                       MPI_COMM_WORLD, stat, err)
+                  if (rank == nprocs - i) then
+                     a = a + a_comm
+                  end if
+               end if
+            end do
+
+         case default
+            stop "Unknown value of flag_compute_mpi"
+      end select
+
+   end subroutine compute_force_wrap
 
    subroutine compute_energy (m, r, v, Ec, Ep, E)
       implicit none
