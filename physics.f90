bcb82f89c8923da1603b41d8b23f063a941d6945
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-16 17:22:18 +0100

    physics: Fix flag_mpi=2 case
    We don’t need to do clever things here, just compute them one by one. ;)

--- physics.f90
+++ physics.f90
@@ -254,25 +254,17 @@ contains
             !    print*, ''
             ! end if
             do i = 0, nprocs - 1
-               a_comm_i = 0._xp
+
                if (i == rank) then
                   r_i = r
                end if
 
                call mpi_bcast(r_i, 3*N, MPI_REAL_XP, i, MPI_COMM_WORLD, err)
 
+               a_comm_i = 0._xp
                call compute_force(m, r_i, 1, N, r, 1, N, a_comm_i)
-               ! print*, a_comm_i
-
-               if (i == rank) then
-                  a = a + a_comm_i
-                  a_comm_i = a
-               else
-                  a = a - a_comm_i
-               end if
 
                call mpi_reduce(a_comm_i, a, 3*N, MPI_REAL_XP, MPI_SUM, i, MPI_COMM_WORLD, err)
-               ! print*, a
 
             end do
 
