15fe391e7c787e562b9d954da7afa26b741f39d6
Corentin Cadiou <contact@cphyc.me>
2016-01-18 17:57:45 +0100

    maxmemory → memory_factor
    THe usage of the new parameter is given as comment in the code

--- constants.f90
+++ constants.f90
@@ -21,8 +21,12 @@ module constants
    integer,  parameter :: dump_freq = 10            ! Frequency at which the system is sampled
 
    logical,  parameter :: flag_diag = .true.        ! Choose force/energy computation subroutine, 0=dummy/1=diag
-   integer,  parameter :: flag_mpi = 1              ! Choose mpi logic, 0=full copy of pos. arrays/1=low mem. impact
-   real,     parameter :: maxmemory = 1.5           ! Only usefull when using flag_mpi=3, this is the max mem you can use
+   integer,  parameter :: flag_mpi = 3              ! Choose mpi logic, 0=full copy of pos. arrays/1=low mem. impact
+   ! The following option describes how much we want to spare memory. If each process
+   ! uses n bytes of memory for its own data, a memory_factor = 1 allow the code to use
+   ! 2*n bytes for the interactions computations. If memory_factor = k, it uses n*(1+1/k) bytes
+   ! this option should be a divider of the number of particles per process
+   integer,  parameter :: memory_factor = 1         ! Only useful
    integer,  parameter :: MASTER = 0                ! Choose which rank should be chosen as master node
 
    integer :: un, una ! Unit numbers for file operation
@@ -31,7 +35,7 @@ module constants
              pi, G, &
              npoints, epsilon2, dt, maxtime, maxiter, &
              dump_freq, flag_diag, flag_mpi, &
-             maxmemory, MASTER, &
+             memory_factor, MASTER, &
              un, una
 
 contains
