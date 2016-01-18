994930a536ec027036e985d7e39b2d5f74a1f821
Corentin Cadiou <contact@cphyc.me>
2016-01-18 18:00:47 +0100

    ua and u are global variables
    They are declared in constants and defined in the main, so we don't need
    to pass it as an argument of the subroutine.

--- io_tools.f90
+++ io_tools.f90
@@ -41,22 +41,22 @@ contains
 
    end subroutine write_dump_headers
 
-   subroutine write_dump (ua, u, iter, Ec, Ep, t, r, v) ! FIXME: We will probably need to split this one when flag_mpi=1 will be fixed
+   subroutine write_dump (iter, Ec, Ep, t, r, v) ! FIXME: We will probably need to split this one when flag_mpi=1 will be fixed
       implicit none
 
       real(xp), intent(in) :: Ec, Ep, t
       real(xp), intent(in) :: r(:,:)
       real(xp), intent(in) :: v(:,:)
-      integer,  intent(in) :: ua, u, iter
+      integer,  intent(in) :: iter
 
       integer :: i
 
-      write (u, '(i16, 10(e16.8e2))') iter, t, Ep, Ec
+      write (un, '(i16, 10(e16.8e2))') iter, t, Ep, Ec
 
-      !write (ua, *) iter
-      !do i = 1, npoints
-      !   write (ua, '(10(e16.8e2))') r(1, i), r(2, i), r(3, i), v(1, i), v(2, i), v(3, i)
-      !end do
+      write (una, *) iter
+      do i = 1, npoints
+        write (una, '(10(e16.8e2))') r(1, i), r(2, i), r(3, i), v(1, i), v(2, i), v(3, i)
+      end do
 
    end subroutine write_dump
 
