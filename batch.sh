b48e80bc070641ffc6aa626804a5172f80d1a2c0
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-19 04:17:14 +0100

    Update tools.
    So, just run jobs.sh, then ssh to mesopsl1 and run batch.sh

--- batch.sh
+++ batch.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p tmp
-for i in $(seq 1 12); do
-    sed "s/OMP_NUM_THREADS=.*/OMP_NUM_THREADS=$i/g" job.slurm |
-	sed "s/job-name=.*/job-name=batch-$i/g" > tmp/job$i.slurm
-    sbatch tmp/job$i.slurm
+sed -i "s/time=12/time=01/g" jobs/i16j*
+sed -i "s/partition=medium/partition=short/g" jobs/i16j*
+
+for file in jobs/*; do
+    sbatch $file
 done
--- jobs.sh
+++ jobs.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-for i in 1 2 4 8 16; do
-    for j in 1 2 4 8; do
-        for k in $(seq 1 $((12/$j))); do
-            echo "$i nodes, $j mpi/node ($(($i*$j)) total), $k OpenMP/mpi ($(($j*$k))/node, $(($i*$j*$k)) total)"
-        done
-    done
-done
+#for i in 1 2 4 8 16; do
+#    for j in 1 2 4 8; do
+#        for k in $(seq 1 $((12/$j))); do
+#            echo "$i nodes, $j mpi/node ($(($i*$j)) total), $k OpenMP/mpi ($(($j*$k))/node, $(($i*$j*$k)) total)"
+#        done
+#    done
+#done
+
+make sync
+
+python jobs.py
+
+rsync -ahz --progress jobs batch.sh mesopsl1.obspm.fr:~
