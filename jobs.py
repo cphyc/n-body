fb91f7d6d052dfa378f7169777f4b706b8080bdd
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-19 03:43:15 +0100

    jobs: Update for new code.
    Mainly flag_mpi → flag_memory.
    
    Also, output params in a params.log file.
    
    Send mails to both of us. That’s 440 mails in your inbox tomorrow
    morning. You’re welcome.

--- jobs.py
+++ jobs.py
@@ -5,13 +5,13 @@ import os
 nodes = [1, 2, 4, 8, 16]
 mpi_per_node = [1, 2, 4, 8]
 flag_diag_arr = ['.true.', '.false.']
-flag_mpi_arr = [0, 1, 2, 3, 4]
+flag_memory_arr = ['.true.', '.false.']
 
 def replace_fun(line, **kwa):
     newLine = re.sub(r'<OMP_NUM_THREADS>', str(kwa['omp_num_threads']), line)
     newLine = re.sub(r'<MPI_PROCS>', str(kwa['mpi_procs']), newLine)
     newLine = re.sub(r'<PROCS_PER_NODE>', str(kwa['procs_per_node']), newLine)
-    newLine = re.sub(r'<FLAG_MPI>', str(kwa['flag_mpi']), newLine)
+    newLine = re.sub(r'<FLAG_MEMORY>', str(kwa['flag_memory']), newLine)
     newLine = re.sub(r'<FLAG_DIAG>', str(kwa['flag_diag']), newLine)
     newLine = re.sub(r'<JOB_NAME>', str(kwa['job_name']), newLine)
     newLine = re.sub(r'<NUM_NODES>', str(kwa['num_nodes']), newLine)
@@ -20,13 +20,13 @@ def replace_fun(line, **kwa):
 if not os.path.isdir('jobs'):
     os.mkdir('jobs')
 counter = 0
-for i, j, diag, mpi in product(nodes, mpi_per_node, flag_diag_arr, flag_mpi_arr):
+for i, j, diag, memory in product(nodes, mpi_per_node, flag_diag_arr, flag_memory_arr):
     for k in range(1, 12//j+1):
         counter += 1
         string = ('{i} nodes, {j} mpi/node, ({ij} total), {k} OpenMP/mpi ({jk}/node, {ijk} total)' +
-                  ', flag_diag={diag}, flag_mpi={mpi} ({counter})')
+                  ', flag_diag={diag}, flag_memory={memory} ({counter})')
         print(string.format(i= i, j= j, k= k, ij=i*j, jk=j*k, ijk=i*j*k, diag=diag,
-                            mpi=mpi, counter=counter))
+                            memory=memory, counter=counter))
 
         with open("job-hybrid.slurm", "r") as f:
             lines = f.readlines()
@@ -36,7 +36,12 @@ for i, j, diag, mpi in product(nodes, mpi_per_node, flag_diag_arr, flag_mpi_arr)
             else:
                 diagstr = ''
 
-            jobName = 'i{}j{}k{}{}mpi{}'.format(i,j,k,diagstr,mpi)
+            if memory == '.true.':
+                memorystr = 'mem'
+            else:
+                memorystr = ''
+
+            jobName = 'i{}j{}k{}{}{}'.format(i,j,k,diagstr,memorystr)
 
         with open("jobs/"+jobName, 'w') as f:
             for line in lines:
@@ -45,7 +50,7 @@ for i, j, diag, mpi in product(nodes, mpi_per_node, flag_diag_arr, flag_mpi_arr)
                                 omp_num_threads=k,
                                 mpi_procs=i*j,
                                 procs_per_node=j,
-                                flag_mpi=mpi,
+                                flag_memory=memory,
                                 flag_diag=diag,
                                 job_name=jobName)
                 f.write(s)
