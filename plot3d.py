0cecafb5b414ffe5d3d6ba14872c92e005b1f87e
Bruno Pagani <bruno.n.pagani@gmail.com>
2016-01-20 10:29:31 +0100

    plot3d: Update, fix

--- plot3d.py
+++ plot3d.py
@@ -17,32 +17,33 @@ args = parser.parse_args()
 
 class AnimatedScatter(object):
     def __init__(self, numpoints):
-        time = numpoints/100
-        dt = 1e-2
+        time = 8
+        dt = 1e-3
+        dump_freq = 10
         nbiter = int(time/dt)
-        self.data = pd.read_csv(args.output, skiprows=[i for i in range(1,1+nbiter*(numpoints+1),numpoints+1)], delim_whitespace=True)
+        self.data = pd.read_csv(args.output, skiprows=[i for i in range(1,1+nbiter*(numpoints+1)//dump_freq,numpoints+1)], delim_whitespace=True)
         self.numpoints = numpoints
 
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111,projection = '3d')
-        self.ani = animation.FuncAnimation(self.fig, self.animate, init_func=self.setup_plot, interval=1, repeat=False)
+        self.ani = animation.FuncAnimation(self.fig, self.animate, frames=nbiter//dump_freq, init_func=self.setup_plot, interval=1, repeat=False)
 
     def setup_plot(self):
         init = 0
         end = self.numpoints
 
-        self.scat = self.ax.scatter(self.data.x[init:end], self.data.y[init:end], self.data.z[init:end], animated=True)
+        self.scat = self.ax.scatter(self.data.x[init:end], self.data.y[init:end], self.data.z[init:end], s=5, animated=True)
 
-        self.ax.set_xlim3d(-2, 2)
-        self.ax.set_ylim3d(-2, 2)
-        self.ax.set_zlim3d(-2, 2)
+        self.ax.set_xlim3d(-1.5, 1.5)
+        self.ax.set_ylim3d(-1.5, 1.5)
+        self.ax.set_zlim3d(-1.5, 1.5)
 
         return self.scat,
 
     def animate(self, i):
         init = i * self.numpoints
         end = (i+1) * self.numpoints
-
+        print('Frame', i)
         self.scat._offsets3d = (self.data.x[init:end], self.data.y[init:end], self.data.z[init:end])
 
         return self.scat,
